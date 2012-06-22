03c521cda992b9750d3c66ae52ccb8cb50d3d845
Luis Miguel Rodriguez <luisrodriguezds@gmail.com>
2012-06-22 11:42:58 -0430

    Fixed relationship within an indicator and its objective

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -2,5 +2,7 @@ class Objective < ActiveRecord::Base
   attr_accessible :name, :perspective_id, :score, :weight
 
   belongs_to :perspective
-  has_and_belongs_to_many :indicators
+  #has_and_belongs_to_many :indicators
+
+  has_many :indicators
 end

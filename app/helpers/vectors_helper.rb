79069b4cda06b044598bc254512aa4ef926e24b1
Luis Miguel Rodriguez <luisrodriguezds@gmail.com>
2012-09-28 16:42:12 -0430

    Fixes vector views

--- app/helpers/vectors_helper.rb
+++ app/helpers/vectors_helper.rb
@@ -125,5 +125,74 @@ module VectorsHelper
     return lay.html_safe
   end
 
+  def normalViewWithOvals(objectives, date)
+    rows = 0 
+    rows = rows + objectives.size + 1
+    objectives.each {|o|
+      rows = rows + o.indicators.size
+    }
+
+    lay = ""
+    lay = lay + '<tr>
+      	  <td style="vertical-align:middle" rowspan="'+rows.to_s+'">'
+    
+    a = link_to image_tag(objectives.first.perspective.title_image), showdashboard_path("objectives", "current", 4, @date.id), :style=>'color:#FFFFFF;'
+
+    lay = lay + a + '</td>'
+    objectives.each{|o|
+      lay = lay + '<tr>'
+        if o == objectives.first
+          lay = lay + '<td rowspan = "'+rows.to_s+'">'
+          lay = lay + '<div class="row-fluid">'
+          i = 0 
+          objectives.each{|o|
+            if i%2 == 0 
+              lay = lay + '<div class="row-fluid">'
+            end
+            i = i + 1
+            lay = lay + '<div class="span6" align="center">'
+            a = link_to image_tag(objectiveStaticButton(o,date), :size => "148x70"), objectiveatdate_path(o.id,@date.id,6), :style=>'color:#FFFFFF;'
+            lay = lay + a
+            lay = lay + '</div>'
+            if i%2 == 0 
+              lay = lay + '</div> <!--fin row--></br>'
+              lay = lay + '<div class="row-fluid">'
+            end
+          }
+          lay = lay + '</div>'
+        lay = lay + '</td>'
+      end
+      lay = lay + '<td>'
+        a = ObjectiveScore.find_by_objective_id_and_scoredate_id(o.id,date.id)
+        lay = lay+ o.acronym.to_s + '</td>' + '<td>'+ "%" + '</td>'
+        lay = lay+ '<td>'+ a.score.to_s + '</td>'
+        lay = lay+ '<td>'+ a.goal.to_s + '</td>'
+        lay = lay+ '<td>'+ a.progress.to_s+ '</td>'
+        lay = lay+ '<td>'+ a.baseline.to_s+ '</td>'
+        lay = lay+ '<td>'+ a.growth.to_s+ '</td></tr>'
+
+        o.indicators.each{|i|
+          ind = IndicatorScore.find_by_indicator_id_and_scoredate_id(i.id,date.id)
+          link = link_to ind.indicator.acronym, indicatoratdate_path(ind.indicator_id,ind.scoredate_id,6), :class => indicatorscorestate(ind), :style=>'color:#FFFFFF;'
+
+          lay = lay + '<tr><td>' + link + '</td>'
+          lay = lay + '<td>' + i.unit + '</td>'
+          lay = lay+ '<td>'+ ind.score.to_s + '</td>'
+          lay = lay+ '<td>'+ ind.goal.to_s + '</td>'
+          lay = lay+ '<td>'+ ind.progress.to_s+ '</td>'
+          lay = lay+ '<td>'+ ind.baseline.to_s+ '</td>'
+          lay = lay+ '<td>'+ ind.growth.to_s+ '</td></tr>'
+        }
+
+
+      lay = lay + '</td>'
+      lay = lay + '</td>'     
+    }
+
+    return lay.html_safe
+  end
+
+
+
 
 end
